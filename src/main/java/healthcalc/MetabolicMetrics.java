629e164b9c12abff289fefa604318ef2924fd09b
GonzaloMA <gonzalo.mesas.aranda@uma.es>
2024-05-27 15:47:09 +0200

    Cambio de las clases _Metrics a interfaces y clase HealthCalcImpl e interfaz HelathCalc

--- src/main/java/healthcalc/MetabolicMetrics.java
+++ src/main/java/healthcalc/MetabolicMetrics.java
@@ -1,26 +1,7 @@
 package healthcalc;
 
-public class MetabolicMetrics {
+public interface MetabolicMetrics {
 
-    public double basalMetabolicRate(Persona persona) {
-        float peso = persona.weight();
-        float altura = persona.height();
-        int edad = persona.age();
-
-        if (peso <= 0 || peso > 500 || altura <= 0 || altura > 250 || edad <= 0 || edad > 150) {
-            throw new IllegalArgumentException("Argumentos no válidos");
-        } else {
-            float resultado;
-            if (persona.gender() == Gender.MALE) {
-                resultado = 10 * peso + 6.25f * altura - 5 * edad + 5;
-            } else {
-                resultado = 10 * peso + 6.25f * altura - 5 * edad - 161;
-            }
-            if (resultado < 0) {
-                throw new IllegalArgumentException("El resultado de la operación es negativo");
-            } else {
-                return resultado;
-            }
-        }
-    }
+    public double basalMetabolicRate(Persona persona);
+    
 }
